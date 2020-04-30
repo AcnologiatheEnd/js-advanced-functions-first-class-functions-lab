926e8162432f45509706a57d37ef72b2c1612347
Winston  Chung <18.chung.winston@ctkprep.org>
2020-04-30 11:27:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers() {
-  Function(drivers) {
-    return 
-  }
+const returnFirstTwoDrivers = function (drivers) {
+  const first = []
+  first.push(drivers[0])
+  firstT.push(drivers[1])
+  return first
 }
