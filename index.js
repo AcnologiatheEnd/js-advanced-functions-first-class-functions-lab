c9ab2e241000603fe7a0b012005ccf90ff68a509
Winston  Chung <18.chung.winston@ctkprep.org>
2020-04-30 11:45:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+  const first = []
+  first.push(drivers[0])
+  first.push(drivers[1])
+  return first
+}
+
+const returnLastTwoDrivers = function (drivers) {
+  const last = []
+  last.push(drivers[drivers.length-2])
+  last.push(drivers[drivers.length-1])
+  return last
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (multiplier) {
+  return function (fare) {
+    return multiplier * fare
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, func) {
+   return func(drivers)
+}
